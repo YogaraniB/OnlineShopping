8041b1d1d652fde3faac5fe2c6b676440a2d1111
Yogarani.B <yogarani2014@gmail.com>
2020-06-20 12:41:35 +0530

    message added

--- OnlineShoppingSpringBoot/src/main/java/com/tvm/OnlineFishMart/OnlineFishMart/web/CustomExHandler.java
+++ OnlineShoppingSpringBoot/src/main/java/com/tvm/OnlineFishMart/OnlineFishMart/web/CustomExHandler.java
@@ -25,21 +25,17 @@ public class CustomExHandler extends ResponseEntityExceptionHandler {
 		ErrorMessage errorMessage=new ErrorMessage(new Date(), ex.getMessage(), "yoga@lnt.com",
 				req.getDescription(false));
 		ResponseAPI error=new ResponseAPI(ex.getMessage(),Boolean.FALSE);
-		return new ResponseEntity<Object>(error, HttpStatus.NOT_FOUND);
-		
+		return new ResponseEntity<Object>(errorMessage, HttpStatus.NOT_FOUND);
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	
 	protected ResponseEntity<Object> handleMethodArgumentNotValid
 	(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
 		ErrorMessage errorMessage=new ErrorMessage
 				(new Date(), "data is not valid please refer doc", "yoga@lnt.com", 
 						request.getDescription(false));
-		
 		return new ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST);
 	}
 	
@@ -48,7 +44,6 @@ public class CustomExHandler extends ResponseEntityExceptionHandler {
 		ErrorMessage errorMessage=new ErrorMessage(new Date(), ex.getMessage(), "yoga@lnt.com",
 				req.getDescription(false));
 		return new ResponseEntity<Object>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-		
 	}	
 	
 	
